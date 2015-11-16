edcfc79e47c939dc9f0182443a94d09af5b19aec
BunnY <w@bunnywong.com>
2015-11-17 00:24:48 +0800

    add new tab to product detail

--- include/Product_FulvicAcidSet.php
+++ include/Product_FulvicAcidSet.php
@@ -14,6 +14,11 @@
             <?php echo t('Description'); ?>
 					</a>
 				</li>
+        <li class="tabIndex">
+          <a>
+            <?php echo t('Benefits'); ?>
+          </a>
+        </li>
 				<li class="tabIndex">
 					<a>
 						<?php echo t('How to use'); ?>
@@ -32,17 +37,15 @@
 				</li>
 			</ul>
 			<!-- tab 1 -->
-			<div class="product_initialShowTab tabContent"><div>
-
-
-
+			<div class="product_initialShowTab tabContent">
 				<p><?php echo t('Box Set is exclusively designed for Mainland China market. For specific needs, please contact us or the local distributor.'); ?></p>
 				<p><?php echo t('Organic ISNATURA purified fulvic acid is extracted with the highest technology allowing the concentration levels to reach up to 80%. We guarantee our fulvic acid concentration to be at least 60%.'); ?></p>
         <p><?php echo t('Fulvic Acid is recognized as one of the key elements in many outstanding health and scientific breakthroughs of the 21st century. Fulvic Acid is extracted from the major organic constituents of the enriched Arizona Desert and Mexico Desert soil, and it is found in Earth’s finest and most nutritious soil layer. Fulvic Acid is known as “the natural detox miracle”.  Due to its special chemical composition, it is one of the most powerful natural antioxidants and free radical scavengers known.');?></p>
         <p><?php echo t('Fulvic acid has the ability to enhance our intake of nutrients allowing our body to absorb the necessary minerals and vitamins for our bodily functions. The greatest function of fulvic acid is its power to aid the body in cell regeneration, boosting your immune system and increasing your recovery rate. Fulvic acid has shown to act as a catalyst in the breakdown of toxic pollutants, aiding the body to detoxify.');?></p>
 				<p><?php echo t('It has been established that fulvic acid is vital in helping to form new species of metal ions, binding with organic pollutants such as pesticides and herbicides, and catalyzing the breakdown of toxic pollutants. Radioactive substances react rapidly with fulvic acid, and only a brief time is required for equilibrium to be reached. All radioactive elements are capable of reacting with fulvic acid and thus forming organo-metal complexes of different adsorptive stability and solubility.');?></p>
-        <span><?php echo t('Benefits'); ?></span>
-        <ul class="font-light text-content">
+			</div>
+      <div class="tabContent">
+        <ul class="text-content">
           <li><?php echo t('Detoxify heavy metal, toxin, pesticide residues and other disease cells'); ?></li>
           <li><?php echo t('Stimulate immune system, intensify wound healing'); ?></li>
           <li><?php echo t('Nature’s antioxidant, resist radiation, enhance anti-aging, improve skin and hair quality'); ?></li>
@@ -50,23 +53,18 @@
           <li><?php echo t('Promote nutrient absorption, improve the quality of sleep, and reduce other health issues risk'); ?></li>
           <?php echo ec('', '<li>改善“三高”、內分泌失調、調整腸胃，便秘等亞健康問題</li>'); ?>
         </ul>
-			</div>
-			</div><!-- end of t1 -->
-			<!-- tab 2 -->
-			<div class="tabContent"><div>
+      </div>
+			<div class="tabContent">
         <p><strong><?php echo t('Nutritional Supplement'); ?></strong>:<span class="x-font-light"><?php echo t('Add 5 droppers (around 2.5 ml) of fulvic acid to 200ml of beverage and consume as normal.  Consume 2 times a day.'); ?></span></p>
         <p><strong><?php echo t('Intensive Detox'); ?></strong>:<span class="x-font-light"><?php echo t('For best results take at least 3-4 times a day.'); ?></span></p>
         <p><strong><?php echo t('Externally'); ?></strong>:<span class="x-font-light"><?php echo t(' Apply to any skin irritations, cuts or wounds directly to reduce inflammation and prevent infection.'); ?></span></p>
         <p><span class="x-font-light"><?php echo t('1) Consult a doctor prior to use if you are pregnant, nursing or taking other medications. Fulvic Acid is not a medicine. Immediately discontinue and consult a doctor if any adverse reactions occur.'); ?></span></p>
         <p><span class="x-font-light"><?php echo t('2) All ingredients are natural extracts, mineral and plant deposits are common.'); ?></span></p>
 			</div>
-			</div><!-- end of t2 -->
-			<!-- tab 3 -->
 			<div class="tabContent">
         <p><?php echo t('There are no side effects to the use of fulvic acid.  However, your body may undertake some changes within the first few weeks of consuming fulvic acid.  You may notice that your urine appears a brighter color, and you may find yourself passing stool more often. Person with excessive toxin in the body might experience rash. This is all very normal. They are signs that the fulvic acid is clearing out the toxins in your system.'); ?></p>
         <p><?php echo t('In addition, the fatigued people appear more energetic, over fatigued people will want to sleep, compulsive sleepiness people will appear with phenomenon of sleepless, these are all very normal phenomenon. This is because the ISNATURA fulvic acid concentration has dual regulatory function which help to improve sleep quality.'); ?></p>
-			</div><!-- end of t3 -->
-			<!-- tab 4 -->
+			</div>
 			<div class="tabContent">
 				<p><?php echo t('Some users have felt that after a few weeks of consuming fulvic acid they are more energetic, increase in food consumptions and metabolism, improvement in sleep quality. Others have noticed a significant increase in nail growth, hair growth, and improvement in hair quality. Fulvic acid benefits are very subtle and are more internal than and external.'); ?></p>
       </div><!-- end of t4 -->
